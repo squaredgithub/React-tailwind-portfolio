7c44d89b9576fe0643a61b3940f23c4f8e9faaa6
squaredgithub <njugunafrancis077@gmail.com>
2024-09-08 19:29:56 +0300

    edited

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { contactImg } from "../../assets/index";
+import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
+
 
 const ContactLeft = () => {
   return (
     <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-4 lgl:p-8 rounded-lg shadow-shadowOne flex flex-col gap-8 justify-center">
-      <img
-        className="w-full h-64 object-cover rounded-lg mb-2"
-        src={contactImg}
-        alt="contactImg"
-      />
       <div className="flex flex-col gap-4">
         <h3 className="text-3xl font-bold text-white">Francis Njuguna</h3>
         <p className="text-lg font-normal text-gray-400">
